7227c21853bebd5dffa8a697add07fd8aee1ced0
Jan Svatos <jansvatos90@gmail.com>
2025-01-14 15:28:39 +0100

    Add toggle switch lib to demo app

--- apps/demo/src/app/app.component.ts
+++ apps/demo/src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { ToggleSwitchComponent } from '@myorg/toggleSwitch';
 @Component({
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    ToggleSwitchComponent,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
