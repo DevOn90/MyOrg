b6783895a51c810edfd61f305c197a1485868151
Jan Svatos <jansvatos90@gmail.com>
2025-01-20 10:05:42 +0100

    Apply dynamic sizeFactor

--- shared/components/toggleSwitch/src/lib/toggleSwitch/toggleSwitch.component.ts
+++ shared/components/toggleSwitch/src/lib/toggleSwitch/toggleSwitch.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -7,4 +7,20 @@ import { CommonModule } from '@angular/common';
   templateUrl: './toggleSwitch.component.html',
   styleUrl: './toggleSwitch.component.scss',
 })
-export class ToggleSwitchComponent {}
+export class ToggleSwitchComponent implements OnInit, OnChanges {
+
+  @Input() size!:number;
+  @HostBinding('style.--sizeFactor') sizeFactor!:string;
+
+  ngOnInit(): void {
+    this.applyDefaultSetting();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.applyDefaultSetting();
+  }
+
+  private applyDefaultSetting() {
+    this.sizeFactor = `${this.size || 1}`; // Use 1 as fallback value
+  }
+}
